8f4dc439bb305f870858319c0baa92e81b96e4a8
tomotele <tomotelechristopher25@gmail.com>
2025-12-16 00:37:01 +0100

    :Feature: Add Progressive Web App

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ import Loader from "@/components/Loader";
 import { useState, useEffect } from "react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/next";
-import PWAInstallPrompt from "@/components/PWAInstallPrompt";
+import PWAInstallPrompt from "../components/PWAInstallPromt";
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
