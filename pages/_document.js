fc0ec19151a07e650e7aa79d139eeec3ca9e0ca6
tomotele <tomotelechristopher25@gmail.com>
2025-12-16 01:53:50 +0100

    :Feature: add ios notice

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,50 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* PWA Manifest */}
         <link rel="manifest" href="/manifest.json" />
+
+        {/* Theme color */}
         <meta name="theme-color" content="#AE2108" />
+
+        {/* iOS Home Screen Icon */}
+        <link rel="apple-touch-icon" href="/icon-512.png" />
+
+        {/* iOS Splash Screens */}
+        <link
+          rel="apple-touch-startup-image"
+          href="/ios-splash/iphone-se.png"
+          media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/ios-splash/iphone-8.png"
+          media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/ios-splash/iphone-8-plus.png"
+          media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/ios-splash/iphone-x.png"
+          media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/ios-splash/iphone-11-pro-max.png"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/ios-splash/ipad-pro.png"
+          media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
+        />
+
+        {/* Optional: prevent zoom & set web app mode */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <link rel="apple-touch-icon" href="/logo.jpg" />
       </Head>
       <body>
         <Main />
